d3bc32a4b67901975dd10ec6fc0d4e11333f63e0
diegozev <136757622+diegozev@users.noreply.github.com>
2023-07-10 13:37:31 -0400

    Update principal2.py

--- principal2.py
+++ principal2.py
@@ -5,14 +5,9 @@ import time
 import sys
 # import camera
 
-cv2.imwrite("fotograma.jpg", frame)   
+sys.path.append('../')
 
-##########################################  VIDEO PUNTO MEDIO  #################
-
-if ret:
-    frame = cv2.resize(frame, (640, 480))
-
-def obtener_fuga(frame):
+def obtener_fuga(frame):    
     ## Elegir puntos para transformación perspectiva REGIÓN DE INTERÉS
     tl = (50,300) #top left
     bl = (5,472) #bottom left 
@@ -39,9 +34,22 @@ def obtener_fuga(frame):
     u_s = 50
     u_v = 255
 
-    lower = np.array([l_h, l_s, l_v])
-    upper = np.array([u_h, u_s, u_v])
-    mask = cv2.inRange(hsv_transformed_frame, lower, upper)
+    # Rango para el blanco
+    lower = np.array([l_h,l_s,l_v])
+    upper = np.array([u_h,u_s,u_v])
+
+    # Rangos de color para la línea amarilla
+    lower_yellow = np.array([15, 50, 100])
+    upper_yellow = np.array([35, 255, 255])
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Máscaras para la línea blanca y la línea amarilla
+    mask_white = cv2.inRange(hsv_transformed_frame, lower, upper)
+    mask_yellow = cv2.inRange(hsv_transformed_frame, lower_yellow, upper_yellow)
+
+    # Unir las máscaras para detectar ambas líneas
+    mask = cv2.bitwise_or(mask_white, mask_yellow)
 
     histogram = np.sum(mask[mask.shape[0] // 2:, :], axis=0)
     midpoint = int(histogram.shape[0] / 2)
@@ -49,6 +57,7 @@ def obtener_fuga(frame):
     right_base = np.argmax(histogram[midpoint:]) + midpoint
     fuga = (right_base + left_base) /2
     distancia = right_base - left_base
+    print(fuga)
     
     if fuga>250 and distancia>100:
         return 0 #derecho
@@ -56,5 +65,4 @@ def obtener_fuga(frame):
         return 2 #curva
     else: #(menor a 250)
         return 1 #enderezarse
-    # print(distancia,' ',fuga)
 
